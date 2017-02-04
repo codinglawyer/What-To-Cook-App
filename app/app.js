432fc93e95856f82589f4b976252156dc856c5d3
David Kopal <kopaldvd@gmail.com>
2017-02-04 21:34:09 +0100

    feat: local storage

--- app/app.js
+++ app/app.js
@@ -6,8 +6,15 @@ import createLogger from 'redux-logger';
 import Recipes from './reducers/recipes';
 import Input from './containers/Input'
 
+let localStorageState = JSON.parse(localStorage.getItem('recipes'));
+localStorageState = localStorageState ? localStorageState : [];
+
 const logger = createLogger();
-let store = createStore(Recipes, applyMiddleware(logger));
+let store = createStore(Recipes, localStorageState, applyMiddleware(logger));
+
+setInterval(() => {
+    localStorage.setItem('recipes', JSON.stringify(store.getState()))
+},1000);
 
 ReactDOM.render(
     <Provider store={store}>
