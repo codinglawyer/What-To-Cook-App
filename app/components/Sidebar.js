401d0e5fb6316512dced7fa3b602e3f0d532968e
David Kopal <kopaldvd@gmail.com>
2017-02-12 21:28:39 +0100

    refactor: sidebar to functional component

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -5,41 +5,35 @@ import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui
 import LeftDrawer from '../components/LeftDrawer'
 
 
-export default class Sidebar extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: 3,
-        };
-    }
-
-    handleChange = (event, index, value) => this.setState({value});
-
-    render() {
-        return (
-            <Toolbar style={{ marginBottom: "50px"}}>
-                <ToolbarTitle
-                    text="What To Cook?"
-                    style={{ fontWeight: 500, fontSize: "25px"}}
+const Sidebar = ({
+    recipes,
+    dispatch,
+    displayRecipeForm,
+    formDisplayed,
+    actions,
+}) => (
+        <Toolbar style={{ marginBottom: "50px"}}>
+            <ToolbarTitle
+                text="What To Cook?"
+                style={{ fontWeight: 500, fontSize: "25px"}}
+            />
+            <ToolbarGroup>
+                <ToolbarSeparator />
+                <RaisedButton
+                    className="newRecipe"
+                    labelStyle={{fontSize: '16px'}}
+                    label="Add A New Recipe"
+                    primary={true}
+                    type="button"
+                    onClick={() => dispatch(actions.displayRecipeForm(!formDisplayed))}
                 />
-                <ToolbarGroup>
-                    <ToolbarSeparator />
-                    <RaisedButton
-                        className="newRecipe"
-                        labelStyle={{fontSize: '16px'}}
-                        label="Add A New Recipe"
-                        primary={true}
-                        type="button"
-                        onClick={() => this.props.dispatch(this.props.actions.displayRecipeForm(!this.props.formDisplayed))}
-                    />
-                    <LeftDrawer
-                        recipes={this.props.recipes}
-                        dispatch={this.props.dispatch}
-                    />
-                    <ToolbarSeparator />
-                </ToolbarGroup>
-            </Toolbar>
-        );
-    }
-}
+                <LeftDrawer
+                    recipes={recipes}
+                    dispatch={dispatch}
+                />
+                <ToolbarSeparator />
+            </ToolbarGroup>
+        </Toolbar>
+    );
+
+export default Sidebar;
