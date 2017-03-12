ef1420075f4bbb935541fcc314d3ef455eac7a71
David Kopal <kopaldvd@gmail.com>
2017-03-12 23:01:03 +0100

    feat: Redux DevTools Extension added

--- app/configureStore.js
+++ app/configureStore.js
@@ -10,6 +10,7 @@ const configureStore = () => {
     const sagaMiddleware = createSagaMiddleware();
     const store = createStore(
         RootReducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
         applyMiddleware(logger, sagaMiddleware)
     );
     sagaMiddleware.run(rootSaga);
