2f8e85d2211a5d27747528fc0414ba0befcab908
David Kopal <kopaldvd@gmail.com>
2017-03-13 18:25:55 +0100

    fix: selector fix

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,6 +3,7 @@ import { reducer as formReducer } from 'redux-form';
 
 import recipesEntity, * as fromRecipes from './recipes';
 import ingredientsEntity, * as fromIngredients from './ingredients';
+import { cloneDeep } from 'lodash'
 
 const RootReducer = combineReducers(
     {
@@ -21,8 +22,27 @@ export const getAllIngredients = state => fromIngredients.getAllIngredients(stat
 
 export const getCompleteRecipes = (state, getRecipes, getIngredients) => {
     let recipes = getRecipes(state);
+    let recipesWithIngredients = cloneDeep(recipes)
     let allIngredients = getIngredients(state);
     let relevantIngredients = recipes.map((recipe) => recipe.ingredients.map(elem => ({ [elem]: allIngredients[elem] })));
-    relevantIngredients.map((ing, i) => recipes[i].ingredients = ing);
-    return recipes;
+    relevantIngredients.map((ing, i) => recipesWithIngredients[i].ingredients = ing);
+    return recipesWithIngredients;
+}
+
+export const getRecipe = (state, recipeId) => {
+    let ingredients = state.ingredientsEntity
+    console.log("INGRE", ingredients);
+    let recipes = state.recipesEntity.byId
+    console.log("RECIPES", recipes);
+    let selectedRecipe = recipes[recipeId]
+    console.log("SELECTED", selectedRecipe);
+    let selected = cloneDeep(selectedRecipe)
+    if (selectedRecipe){
+        let relevantIngredients = selected.ingredients.map(ingredient => {console.log("ING", ingredient); return ingredients[ingredient] })
+        // selectedRecipe.ingredients = relevantIngredients
+        console.log("RELE", relevantIngredients);
+        console.log("FINAL", selected);
+        console.log("SEMIFILENA", selectedRecipe);
+        return selected
+    }
 }
