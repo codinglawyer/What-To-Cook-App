5ac4ff25e00c0d53e16cbc27145b741760232d8f
David Kopal <kopaldvd@gmail.com>
2017-12-05 09:09:21 +0100

    add loader when recipe is saving or deleting

--- app/screens/HomeScreen/index.js
+++ app/screens/HomeScreen/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import T from 'prop-types'
-import { get as g } from 'lodash'
+import { get as g, isEmpty } from 'lodash'
 import { connect } from 'react-redux'
 import { compose, withHandlers } from 'recompose'
 import ReactLoading from 'react-loading'
@@ -8,6 +8,8 @@ import { isDataBeingFetched } from '../../actions/index'
 import {
   getAllRecipes,
   getIsDataFetching,
+  getIsRecipeDeleting,
+  getIsRecipeSaving,
   getCompleteRecipes,
   getAllIngredients
 } from '../../reducers/index'
@@ -17,39 +19,45 @@ import { Screen } from '../../styles/global-styles'
 
 const mapStateToProps = state => {
   const dataFetchingInfo = getIsDataFetching(state)
+  const recipeDeletingInfo = getIsRecipeDeleting(state)
+  const recipeSavingInfo = getIsRecipeSaving(state)
   return {
     recipes: getCompleteRecipes(state, getAllRecipes, getAllIngredients),
     isFetching: g(dataFetchingInfo, 'fetching'),
-    errorMessage: g(dataFetchingInfo, 'error')
+    isDeleting: g(recipeDeletingInfo, 'deleting'),
+    isSaving: g(recipeSavingInfo, 'deleting'),
+    fetchError: g(dataFetchingInfo, 'error')
   }
 }
 
 const renderHomeScreen = ({
   recipes,
   isFetching,
-  errorMessage,
+  isDeleting,
+  isSaving,
+  fetchError,
   handleIsDataBeingFetched
 }) => (
-    <Screen>
-      {isFetching && !g(recipes, 'allIds') ? (
-        <ReactLoading type="bars" color="#444" className="createLoader" />
-      ) : (
+  <Screen>
+    {(isFetching && !g(recipes, 'allIds')) || isDeleting || isSaving ? (
+      <ReactLoading type="bars" color="#444" className="createLoader" />
+    ) : (
+      <div>
+        <SiteTitle>What do you want to cook?</SiteTitle>
+        <HeaderPicture />
         <div>
-          <SiteTitle>What do you want to cook?</SiteTitle>
-          <HeaderPicture />
-          <div>
-            {errorMessage &&
-              !g(recipes, 'allIds') && (
-                <FetchError
-                  message={errorMessage}
-                  onRetry={() => handleIsDataBeingFetched()}
-                />
-              )}
-          </div>
+          {!isEmpty(fetchError) &&
+            !g(recipes, 'allIds') && (
+              <FetchError
+                message={fetchError}
+                onRetry={() => handleIsDataBeingFetched()}
+              />
+            )}
         </div>
-      )}
-    </Screen>
-  )
+      </div>
+    )}
+  </Screen>
+)
 
 const HomeScreen = compose(
   connect(mapStateToProps),
@@ -62,7 +70,9 @@ const HomeScreen = compose(
 HomeScreen.propTypes = {
   recipes: T.object,
   isFetching: T.bool,
-  errorMessage: T.object,
+  isSaving: T.bool,
+  isDeleting: T.bool,
+  fetchError: T.object,
   handleIsDataBeingFetched: T.func
 }
 
