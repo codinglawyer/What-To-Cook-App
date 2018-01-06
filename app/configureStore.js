d02d1e67d00a8a837e2ddc3dc17a5cbbbc50a83b
David Kopal <kopaldvd@gmail.com>
2018-01-06 17:33:42 +0100

    clean package json

--- app/configureStore.js
+++ app/configureStore.js
@@ -1,17 +1,15 @@
 import { createStore, applyMiddleware } from 'redux'
-import createLogger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 import RootReducer from './reducers/index'
 import rootSaga from './sagas/sagas'
 
 const configureStore = () => {
-  const logger = createLogger()
   const sagaMiddleware = createSagaMiddleware()
   const store = createStore(
     RootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
       window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(logger, sagaMiddleware)
+    applyMiddleware(sagaMiddleware)
   )
   sagaMiddleware.run(rootSaga)
   return store
