e9b5ed28057f0168a5a4bf14906cc4f9a7eb4e53
David Kopal <kopaldvd@gmail.com>
2018-01-14 21:41:18 +0100

    create Spinner component

---
+++ app/components/Spinner.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactLoading from 'react-loading'
+
+const Spinner = () => (
+  <ReactLoading type="bars" color="#444" className="createLoader" />
+)
+
+export default Spinner
