70590c266ba3d20ad24ff6745cad277e79f65646
David Kopal <kopaldvd@gmail.com>
2018-01-19 21:43:29 +0100

    fix database update saga

--- app/sagas/databaseUpdate.js
+++ app/sagas/databaseUpdate.js
@@ -1,19 +1,23 @@
 import firebaseApp from '../api/firebase'
 import { eventChannel, buffers } from 'redux-saga'
+import { isEmpty } from 'lodash'
 import { put, take } from 'redux-saga/effects'
-import { fetchDataSuccess, fetchDataFailure } from '../actions/index'
+import { fetchDataSuccess, fetchDataFailure } from '../actions'
 
 export default function * databaseUpdate () {
   const chan = eventChannel(emit => {
     const connectionRef = firebaseApp.database().ref()
-    connectionRef.on('value', snapshot => emit(snapshot.val()))
+    connectionRef.on('value', snapshot => {
+      // emitted value can't be null when there are no recipes
+      emit(snapshot.val() || {})
+    })
     // don't stop the channel
     return () => {}
   }, buffers.expanding(1))
 
   while (true) {
     let data = yield take(chan)
-    if (data) {
+    if (!isEmpty(data)) {
       yield put(fetchDataSuccess(data))
     } else {
       yield put(fetchDataFailure(data))
